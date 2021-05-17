703517f8bd6b00de701d37721d75395de1dfcd55
qhd933 <846080491@qq.com>
2021-05-17 19:51:27 +0800

    完成了登录功能

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <router-view></router-view>    
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-  }
-}
+  name: "app",
+  components: {},
+};
 </script>
 
 <style>
-
+@import url("./assets/css/global.css");
+@import url("./assets/fonts/iconfont.css");
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,18 @@
 <template>
-  <div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      Header
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
     <el-container>
-      <!-- 头部区域 -->
-      <el-header
-        >Header <el-button type="info" @click="logout"></el-button
-      ></el-header>
-      <!-- 页面主体区域 -->
-      <el-container>
-        <!-- 侧边栏 -->
-        <el-aside width="200px">Aside</el-aside>
-        <!-- 右侧内容主体 -->
-        <el-main>Main</el-main>
-      </el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
     </el-container>
-  </div>
+  </el-container>
 </template>
 
 <script>
@@ -28,4 +27,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  // width: 100vh;
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
