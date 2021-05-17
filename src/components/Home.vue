dc524c3430c4e8a8e1f49c1c521016c1181323a2
qhd933 <846080491@qq.com>
2021-05-17 10:25:04 +0800

    完成了登录功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>home</div>
+  <div>
+    <el-container>
+      <!-- 头部区域 -->
+      <el-header
+        >Header <el-button type="info" @click="logout"></el-button
+      ></el-header>
+      <!-- 页面主体区域 -->
+      <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="200px">Aside</el-aside>
+        <!-- 右侧内容主体 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.removeItem("token");
+      this.$router.push("/login");
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
