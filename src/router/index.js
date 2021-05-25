22911107c59c5fdb104c7386ec45684823485a14
qhd933 <846080491@qq.com>
2021-05-25 11:18:46 +0800

    完成了报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ const Params = () => import('../components/goods/Params.vue')
 const List = () => import('../components/goods/List.vue')
 const Add = () => import('../components/goods/Add.vue')
 const Order = () => import('../components/order/Order.vue')
+const Report = () => import('../components/report/Report.vue')
 
 Vue.use(VueRouter)
 
@@ -64,6 +65,10 @@ const routes = [
      {
       path:'/orders',
       component:Order
+    },
+    {
+      path:'/reports',
+      component:Report
     }
   ]
  }
